1ac617f0d043ab7f8d73d99cd105a0e52f6924e2
Amir Zakirov <knyazamir@gmail.com>
2026-07-20 20:23:18 +0300

    Added programmierer

---
+++ testAsm/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+FILE="test"
+
+echo "=== Сборка файла $FILE.asm ==="
+nasm -f elf64 $FILE.asm -o $FILE.o
+
+if [ $? -eq 0 ]; then
+    ld $FILE.o -o $FILE
+    
+    if [ $? -eq 0 ]; then
+        echo "=== Сборка успешна! Запуск бинарного файла $FILE ==="
+        ./$FILE
+    else
+        echo "Ошибка на этапе компоновки (ld)"
+    fi
+else
+    echo "Ошибка на этапе компиляции (nasm)"
+fi
+
