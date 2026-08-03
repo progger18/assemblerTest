140a01ca993d9c3469ea3fad13e89fdf85d90c93
Amir Zakirov <knyazamir@gmail.com>
2026-08-03 20:59:31 +0300

    Variables2

--- testAsm/build.sh
+++ testAsm/build.sh
@@ -1,19 +1,34 @@
-#!/bin/bash
-FILE="test_4"
-
-echo "=== Сборка файла $FILE.asm ==="
-nasm -f elf64 $FILE.asm -o $FILE.o
-
-if [ $? -eq 0 ]; then
-    ld $FILE.o -o $FILE
-    
-    if [ $? -eq 0 ]; then
-        echo "=== Сборка успешна! Запуск бинарного файла $FILE ==="
-        ./$FILE
-    else
-        echo "Ошибка на этапе компоновки (ld)"
-    fi
-else
-    echo "Ошибка на этапе компиляции (nasm)"
+if [ -z "$1" ]; then
+	echo "Использование: $0 имя_файла.asm"
+	exit 1
 fi
+
+if [ ! -f "$1" ]; then
+	echo "Ошибка: Файл '$1' не найден"
+	exit 1
+fi
+
+FILENAME=$(basename "$1" .asm)
+
+echo "[1/3] Компиляция $FILENAME.asm..."
+nasm -f elf64 "$1" -o "$FILENAME.o"
+if [ $? -ne 0 ]; then
+	echo "Ошибка компиляции!"
+	exit 1
+fi
+
+echo "[2/3] Компоновка FILENAME.o..."
+ld "$FILENAME.o" -o "$FILENAME"
+if [ $? -ne 0]; then
+	echo "Ошибка компоновки!"
+	exit 1
+fi
+
+rm -f "FILENAME.o"
+
+echo "[3/3] Запуск ./$FILENAME:"
+echo "------------------------------------"
+"./$FILENAME"
+echo "------------------------------------"
+echo "Выполнение завершено. Код возврата: $?"
 
